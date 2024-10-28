982c14cddb92b631dd94e4b13f33bde5cc6c241d
CongLam1806 <lamtc1806@gmail.com>
2024-10-28 19:05:14 +0700

    update

---
+++ MobileProgramming/MobileProgramming.Business/UseCase/Cart/Queries/GetCartByUserId/GetCartByUserIdQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using MobileProgramming.Business.Models.Response;
+using MobileProgramming.Business.Models.ResponseMessage;
+using MobileProgramming.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileProgramming.Business.UseCase
+{ 
+    public class GetCartByUserIdQueryHandler : IRequestHandler<GetCartByUserIdQuery, APIResponse>
+    {
+        private readonly ICartRepository _cartRepository;
+
+        public GetCartByUserIdQueryHandler(ICartRepository cartRepository)
+        {
+            _cartRepository = cartRepository;
+        }
+
+        public async Task<APIResponse> Handle(GetCartByUserIdQuery request, CancellationToken cancellationToken)
+        {
+            var response = new APIResponse();
+            var cart = await _cartRepository.GetActiveCartByUserIdAsync(request.UserId);
+
+            response.StatusResponse = HttpStatusCode.OK;
+            response.Message = MessageCommon.GetSuccesfully;
+            response.Data = cart;
+
+            return response;
+        }
+    }
+}
